bc711f0d76a7192922b965b48f2e817f452447a7
dwilley21 <daltonwilley@gmail.com>
2020-01-12 00:05:47 -0600

    Minor styling changes

--- src/UI/HomeNav.js
+++ src/UI/HomeNav.js
@@ -6,8 +6,6 @@ import Order from '../Main/Order';
 import About from '../Main/About';
 import ContactForm from '../Forms/ContactForm';
 
-
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,38 +13,44 @@ import {
   Link
 } from "react-router-dom";
 
+const FlexIt =styled.div `
+ display: flex; 
+ flex-direction: row;
+ justify-content: center;
+`
+const Tags = styled.div `
+  padding: 10px; 
+  hover{
+    font-size: 100px;
+  }
+`
 function HomeNav() {
   return (
     <>
     <Router>
         <nav>
-          <ul>
-            <div>
+          <FlexIt>
+            <Tags>
               <Link to="/">Home</Link>
-            </div>
-            <div>
+            </Tags>
+            <Tags>
               <Link to="/About">About</Link>
-            </div>
-            <div>
+            </Tags>
+            <Tags>
               <Link to="/Order">Order</Link>
-            </div>
-            <div>
+            </Tags>
+            <Tags>
               <Link to="/Services">Services</Link>
-            </div>
-            <div>
+            </Tags>
+            <Tags>
               <Link to="/Contact">Contact</Link>
-
-              <Route exact path='/' component={HomeScreen}/>
+            </Tags>          
+          </FlexIt>
+          <Route exact path='/' component={HomeScreen}/>
               <Route exact path='/Services' component={Services}/>
               <Route exact path='/Order' component={Order}/>
               <Route exact path='/Contact' component={ContactForm}/>
               <Route exact path='/About' component={About}/>
-
-
-
-
-            </div>
-          </ul>
         </nav>
         </Router>
     </>
